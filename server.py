768907d0ef5b9fb0d16c2d8f747f9a7deb38e060
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2016-08-12 16:23:22 -0700

    fixed syntax error with trailing comma

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db, 
+from model import connect_to_db, db
 
 
 app = Flask(__name__)
