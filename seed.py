14719aa86a9ff24ab3d1dc40cb803e275550717b
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2016-08-13 11:28:58 -0700

    saving before obliterating to start over

--- seed.py
+++ seed.py
@@ -1,13 +1,15 @@
 import datetime
 from sqlalchemy import func
 
-from model import Event, connect_to_db, db
+from model import Event, EventFile, connect_to_db, db
+from sqlalchemy.orm.exc import NoResultFound
 from server import app
 from datetime import datetime
 from bs4 import BeautifulSoup
 import re
 import zipfile
 import requests
+import os
 
 
 DATADIR = "data/"
@@ -15,8 +17,42 @@ GDELT_DOWNLOAD_URL = 'http://data.gdeltproject.org/events/'
 GDELT_DOWNLOAD_LIST_PAGE = 'index.html'
 GDELT_DOWNLOAD_FILE_RE = r'^201\d+.*\.zip$' # only get files with dates in the 2010s
 
+def add_zipfiles_to_db():
+  """add already-downloaded files (from before I was tracking in db) to db"""
+
+  print "GETTING FILES ALREADY DOWNLOADED"
+
+  for filename in os.listdir(DATADIR):
+    if re.match(GDELT_DOWNLOAD_FILE_RE, filename):
+      try:
+        file = EventFile.query.filter(EventFile.zipfile_name == filename).one()
+        file.downloaded = True
+        db.session.commit()
+
+      except NoResultFound:
+        file = EventFile(zipfile_name=filename, downloaded=True)
+        db.session.add(file)
+        print "added {} to db as downloaded".format(filename)
+
+  db.session.commit()
+
+
+def update_gdelt_stragglers():
+  """ran out of disk space; db and disk got out of sync"""
+
+  for filename in os.listdir(DATADIR):
+    if re.match(GDELT_DOWNLOAD_FILE_RE, filename):
+      try:
+        file = EventFile.query.filter(EventFile.zipfile_name == filename).one()
+        file.unzipped = False
+        db.session.commit()
+      except:
+        print "*********couldn't find {} in db".format(filename)
+
+
+
 def get_gdelt_files():
-  """get list of gdelt files from gdelt download site, return list"""
+  """get list of gdelt files from gdelt download site, add to gdelt_files table"""
 
   # get file list
   print "getting file list"
@@ -26,51 +62,94 @@ def get_gdelt_files():
   soup = BeautifulSoup(r.content, 'html.parser')
   links = soup.find_all('a')
 
-  relevant_link_list = []
-
   for link in links:
     link_text = link.get_text()
+
     if re.match(GDELT_DOWNLOAD_FILE_RE, link_text):
-      relevant_link_list.append(link_text)
+      try:
+        file = EventFile.query.filter(EventFile.zipfile_name == link_text).one()
+        print "{} already in db. Skipping.".format(link_text)
 
-  return relevant_link_list
+      except NoResultFound: 
+        file = EventFile(zipfile_name=link_text)
+        db.session.add(file)
 
-def process_gdelt_files(file_list):
-  """download and unzip each file in the argument list, and add contents to the db"""
+  db.session.commit()
+
+
+def download_gdelt_files():
+  """download each file in the gdelt_files table"""
 
   # download and unzip each file
-  for name in file_list:
-    print "downloading", name
+  for file in EventFile.query.all():
 
-    r = requests.get(GDELT_DOWNLOAD_URL + name)
+    name = file.zipfile_name
 
-    filepath = DATADIR + name
+    # in case this is running after having been partially run before,
+    # skip any already-downloaded files.
 
-    # write to file
-    with open(filepath, "wb") as code:
-      code.write(r.content)
+    if file.downloaded:
+      print "{} already downloaded. Skipping.".format(name)
 
-    # unzip the file
-    zip_ref = zipfile.ZipFile(filepath, 'r')
-    zip_contents = zip_ref.namelist()
+    else: 
+      print "downloading", name
 
-    # if there's more than one file, we're in trouble
-    if len(zip_contents) != 1:
-      print "***************{} contains {} archives. Skipping.".format(filepath, len(zip_contents))
-      continue
+      filepath = DATADIR + name
+      r = requests.get(GDELT_DOWNLOAD_URL + name)
 
-    zip_ref.extractall(DATADIR)
-    zip_ref.close()
+      # write to file
+      with open(filepath, "wb") as code:
+        code.write(r.content)
 
-    # we've already checked that there's only one file
-    unzipped_filepath = zip_contents[0]
+      file.downloaded = True
 
-    # open the file and add the contents to the db
-    add_to_db(DATADIR + unzipped_filepath)
+      # commit here instead of end of function, in case script gets interrupted
+      db.session.commit()
+  
+
+def unzip_gdelt_files():
+    """unzip each .zip file in the data dir"""
+
+    for file in EventFile.query.all():
+
+      name = file.zipfile_name
+
+      if file.unzipped:
+        print "{} already unzipped. Skipping.".format(name)
+
+      else:
+        filepath = DATADIR + name
+
+        # unzip the file
+        zip_ref = zipfile.ZipFile(filepath, 'r')
+        zip_contents = zip_ref.namelist()
+
+        # if there's more (or less) than one file, we're in trouble
+        if len(zip_contents) != 1:
+          print "***************{} contains {} archives. Skipping.".format(filepath, len(zip_contents))
+          continue
+
+        zip_ref.extractall(DATADIR)
+        zip_ref.close()
+
+        # rm the zip file for disk space
+        try:
+          os.remove(DATADIR + name)
+        except:
+          print "*********could not remove file {}".format(name)
+
+        # we've already checked that there's only one file
+        file.csvfile_name = zip_contents[0]
+        file.unzipped = True
+
+        # commit here instead of end of function, in case script gets interrupted
+        db.session.commit()
+
+        add_to_db(name)
 
 
-def add_to_db(filepath):
-    """parse file contents and add data to db"""
+def add_to_db(zipfile_name):
+    """parse file contents for file param and add data to db"""
     
     # indexes taken from these files: 
     # http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt
@@ -78,59 +157,85 @@ def add_to_db(filepath):
 
     # reference: 
     # http://data.gdeltproject.org/documentation/GDELT-Data_Format_Codebook.pdf
-    print "processing", filepath
-    num_notprocessed = 0
-    num_processed = 0
 
-    with open(filepath) as f:
-      for line in f:
-        tokens = line.split('\t')
+    file = 
 
-        # for i, token in enumerate(tokens):
-        #   print i, token
+      name = file.csvfile_name
 
-        # exit()
+      if file.processed:
+        print "{} already processed. Skipping.".format(name)
 
-        # the spec changed April 1, 2013, but it didn't affect any of these 
-        # fields
-        eid = tokens[0]
-        date = datetime.strptime(tokens[1], '%Y%m%d')
-        ecode = tokens[26]
-        goldstein = float(tokens[30])
-        num_mentions = int(tokens[31])
-        try:
-          lat = float(tokens[53])
-          lng = float(tokens[54])
-          num_processed += 1
-        except:
-          num_notprocessed += 1
-          continue
+      else:
+        filepath = DATADIR + name
+        print "processing", filepath
 
-        evt = Event(gdelt_id=eid, 
-                    event_date=date, 
-                    event_code=ecode, 
-                    goldstein=goldstein,
-                    num_mentions=num_mentions,
-                    lat=lat,
-                    lng=lng)
+        # process line by line
+        with open(filepath) as f:
+          for line in f:
+            process_line(line)
 
-        db.session.add(evt)
+        file.processed = True
+        db.session.commit()
 
-    db.session.commit()
-    print "finished processing {}. Processed lines: {} Not processed lines: {}\n".format(filepath, num_processed, num_notprocessed)
 
+      # rm the zip file for disk space
+      try:
+        os.remove(DATADIR + name)
+      except:
+        print "*********could not remove file {}".format(name)
+
+
+def process_line(line):
+  """add an event based on a gdelt file line"""
+
+  tokens = line.split('\t')
+
+  # the spec changed April 1, 2013, but it didn't affect any of these 
+  # fields
+  eid = tokens[0]
+  date = datetime.strptime(tokens[1], '%Y%m%d')
+  ecode = tokens[26]
+  try:
+    # don't fall over if one of these isn't populated; just move on
+    goldstein = float(tokens[30])
+    num_mentions = int(tokens[31])
+    lat = float(tokens[53])
+    lng = float(tokens[54])
+  except:
+    return
+
+  evt = Event(gdelt_id=eid, 
+              event_date=date, 
+              event_code=ecode, 
+              goldstein=goldstein,
+              num_mentions=num_mentions,
+              lat=lat,
+              lng=lng)
+
+  db.session.add(evt)
 
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # start with a clean slate
-    db.drop_all()
+    # create tables if they don't exist
     db.create_all()
 
-    # files_to_download = get_gdelt_files()
-    # process_gdelt_files(files_to_download)
+    # only run this once
+    # print "*"*10, "ADDING DOWNLOADED ZIPFILES TO DB"
+    # add_zipfiles_to_db()
 
+    # print "*"*10, "GETTING FILE LIST"
+    # get_gdelt_files()
 
-    add_to_db('data/20160811.export.CSV')
+    # print "*"*10, "DOWNLOADING"
+    # download_gdelt_files()
 
-    db.session.commit()
+    print "*"*10, "ADDING UNZIPPED STRAGGLERS"
+    update_gdelt_stragglers()
+
+    print "*"*10, "UNZIPPING"
+    unzip_gdelt_files()
+
+    print "*"*10, "PROCESSING"
+    add_to_db()
+
