daa64efee34b47739b7bfbbb7c21cd66664f1650
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2016-08-14 18:08:56 -0700

    tweaked colors more to my liking

--- static/js/google_mapstyles.js
+++ static/js/google_mapstyles.js
@@ -1,1 +1,62 @@
-var MAPSTYLES = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#F7F5F2"},{"weight":0.8}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#F7F5F2"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#DDD4CB"}]},{"featureType":"poi","elementType":"all","stylers":[{"color":"#DDD4CB"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"}]}]
+// adapted from https://snazzymaps.com/style/118/travel-countries
+
+"use strict";
+
+var landColor = "#F7F7FF"
+var waterColor = "#CCCCCF"
+
+var MAPSTYLES = 
+  [{
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  }, {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "visibility": "on"
+    }, {
+      "color": landColor
+    }, {
+      "weight": 0.8
+    }]
+  }, {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [{
+      "color": landColor
+    }]
+  }, {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [{
+      "color": waterColor
+    }]
+  }, {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [{
+      "color": waterColor
+    }]
+  }, {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  }, {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  }, {
+    "featureType": "all",
+    "elementType": "labels",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  }
+]
